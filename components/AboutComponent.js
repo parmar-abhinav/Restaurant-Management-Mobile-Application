c3b2439d3261b8d056938e2e7de2fefe599154e6
parmar-abhinav <abhi171b010@gmail.com>
2020-05-01 10:07:29 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Card, ListItem } from 'react-native-elements';
 import { Text, ScrollView, FlatList } from 'react-native';
-import { LEADERS } from '../shared/leaders';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -42,19 +42,43 @@ class About extends Component {
                 />
             );
         }
-
-        return (
-            <ScrollView>
-                <History />
-                <Card title="Corporate Leadership">
-                    <FlatList
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                    <FlatList 
                         data={this.props.leaders.leaders}
                         renderItem={RenderLeader}
                         keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </ScrollView>
-        );
+                        />
+                    </Card>
+                </ScrollView>
+            );
+        }
     }
 }
 
--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -3,6 +3,7 @@ import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -15,21 +16,34 @@ const mapStateToProps = state => {
 
 function RenderItem(props) {
     const item = props.item;
-    if (item != null) {
-        return (
-            <Card
-                featuredTitle={item.name}
-                featuredSubtitle={item.designation}
-                image={{uri: baseUrl + item.image}}
-            >
-                <Text style={{ margin: 10 }}>
-                    {item.description}
-                </Text>
-            </Card>
-        )
+    if (props.isLoading) {
+        return(
+                <Loading />
+        );
     }
-    else {
-        return (<View></View>)
+    else if (props.errMess) {
+        return(
+            <View> 
+                <Text>{props.erreMess}</Text>
+            </View>
+        );
+    }
+    else {        
+        if (item != null) {
+            return(
+                <Card
+                    featuredTitle={item.name}
+                    featuredSubtitle={item.designation}
+                    image={{uri: baseUrl + item.image}}>
+                    <Text
+                        style={{margin: 10}}>
+                        {item.description}</Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
     }
 }
 
@@ -43,9 +57,18 @@ class Home extends Component {
     render() {
         return (
              <ScrollView>
-                <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]} />
-                <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]} />
-                <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]} />
+                <RenderItem item={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    isLoading={this.props.dishes.isLoading}
+                    erreMess={this.props.dishes.erreMess} 
+                    />
+                <RenderItem item={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    isLoading={this.props.promotions.isLoading}
+                    erreMess={this.props.promotions.erreMess} 
+                    />
+                <RenderItem item={this.props.leaders.leaders.filter((leader) => leader.featured)[0]}
+                    isLoading={this.props.leaders.isLoading}
+                    erreMess={this.props.leaders.erreMess} 
+                    />
             </ScrollView>
         );
     }
